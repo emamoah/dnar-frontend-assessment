e2716c322e331c5b77a9ada1e920ac4839ca2ee6
Emmanuel Amoah <emma.amoah.jr@gmail.com>
2021-07-31 17:17:21 +0100

    Included chart in coin statistics page.

--- src/pages/CoinInfo/components/Chart/Chart.jsx
+++ src/pages/CoinInfo/components/Chart/Chart.jsx
@@ -1,6 +1,15 @@
 import styles from './Chart.module.scss';
 import api from 'util/api';
 import cx from 'util/shared/classnames';
+import {
+  ResponsiveContainer,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Line,
+  Tooltip
+} from 'recharts';
 
 export default function Chart(props) {
   const coin_id = props._id;
@@ -8,6 +17,8 @@ export default function Chart(props) {
   const percentageChange24h = data?.market_data.price_change_percentage_24h;
   const isPositive = percentageChange24h > 0;
   const changeSign = isPositive ? '+' : '';
+
+  const { data: chartData } = api.useGet(`/coins/${coin_id}/market_chart?vs_currency=usd&days=30&interval=daily`);
 
   return (
     <div className={styles.container}>
@@ -22,7 +33,33 @@ export default function Chart(props) {
           })
         }>{data && changeSign}{percentageChange24h}%</span>
       </div>
-      <div className={styles.container__chart}></div>
+      <div className={styles.container__chart}>
+        <div className={styles.container__chart__content}>
+          <ResponsiveContainer>
+            <LineChart
+              data={chartData?.prices.map(([date, Price]) => ({ date: new Date(date).toDateString(), Price: Price.toFixed(2) }))}
+            >
+              <CartesianGrid strokeDasharray="2 5" stroke="#B1AEB880" vertical={false} />
+              <XAxis dataKey="date" stroke="#B1AEB880" />
+              <YAxis stroke="#B1AEB880" />
+              <Tooltip
+                cursor={{ stroke: '#6800D6' }}
+                itemStyle={{ backgroundColor: '#6800D6', color: '#FFFC' }}
+                contentStyle={{
+                  backgroundColor: '#6800D6',
+                  color: '#FFFC',
+                  border: 0,
+                  borderRadius: '.3em',
+                  padding: '.5em 1em',
+                  fontWeight: '500',
+                  textAlign: 'center'
+                }}
+              />
+              <Line type="monotone" dataKey="Price" stroke="#6800D6"></Line>
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     </div>
   );
 }
